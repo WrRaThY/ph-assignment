f8da0d9446520a3fde7b958c7b18ed85ded3b063
Radoslaw Domanski <RadoslawDomanskiDev@gmail.com>
2018-04-05 00:03:35 +0200

    init + minor frontend config

---
+++ backend/src/main/java/priv/rdo/propellerhead/Application.java
@@ -0,0 +1,12 @@
+package priv.rdo.propellerhead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
